e9834346f35194280b0c358891dc71655280cfa5
geekcourses <geekcourses@gmail.com>
2025-11-14 20:29:24 +0200

    Up: day 3

---
+++ pages/themes/PythonBasics/examples/tmp.py
@@ -0,0 +1,3 @@
+numbers = [12, 7, 23, 8, 15, 30, 4, 18]
+evens = [num for num in numbers if num % 2 == 0]
+print(len(evens))
--- pages/themes/PythonForAI_FromScratch/examples/tmp.py
+++
@@ -1,4 +0,0 @@
-whole_number = 10
-decimal_number = 2.5
-print(type(whole_number))
-print(type(decimal_number))
